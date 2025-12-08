8423d5738587e9f380affde926ddcc6c0c96376b
vanh1604 <VietAnhScout@gmail.com>
2025-12-08 22:22:32 +0700

    update: authRoute and authController updated user location

--- controller/authController.js
+++ controller/authController.js
@@ -46,4 +46,22 @@ const signIn = async (req, res) => {
   }
 };
 
-export { signUp, signIn };
+const updateLocation = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { state, city, locality } = req.body;
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      { state, city, locality },
+      { new: true }
+    );
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    return res.status(200).json({ updatedUser });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export { signUp, signIn, updateLocation };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signIn, signUp } from "../controller/authController.js";
+import { signIn, signUp, updateLocation } from "../controller/authController.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/api/signup", signUp);
 authRouter.post("/api/signin", signIn);
+authRouter.put("/api/users/:id", updateLocation);
 export default authRouter;
